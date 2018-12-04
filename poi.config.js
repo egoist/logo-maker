7544f6e495d2c5a3e26d2f58ac8c86530da2d932
EGOIST <0x142857@gmail.com>
2018-12-04 18:41:13 +0800

    it's annoying to run linter during development

--- poi.config.js
+++ poi.config.js
@@ -2,11 +2,6 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   entry: 'src/index',
-  plugins: [
-    {
-      resolve: '@poi/plugin-eslint'
-    }
-  ],
   chainWebpack(config) {
     if (isProd) {
       config.plugin('offline').use(require('offline-plugin'), [
